9b7ec47341168190cc5cae66768f8d1694493012
vasanthram7 <vramsjec@gmail.com>
2019-05-09 15:53:35 +0530

    flask -advance

--- config/prod.py
+++ config/prod.py
@@ -4,3 +4,9 @@ SECRET_KEY = 'topsecret'
 #SQLALCHEMY_DATABASE_URI='postgresql://postgres:topsecret@localhost/catalog_db'
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+# DEBUG = False
+# SECRET_KEY = 'topsecret'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123@localhost/catalog_db'
+# SQLALCHEMY_TRACK_MODIFICATIONS = False
