c10b6ac47123dd5ffe83b596dd83205403faacb4
vasanthram7 <vramsjec@gmail.com>
2019-05-10 17:59:43 +0530

    adding try catch block in run.py

--- run.py
+++ run.py
@@ -1,13 +1,16 @@
 from app import create_app, db  # app/__init__ (ROOT PACKAGE)
 from app.auth.models import User
+from sqlalchemy import exc
 
 #if __name__ == '__main__':
 flask_app = create_app('prod')
 with flask_app.app_context():
         db.create_all()
-        if not User.query.filter_by(user_name='harry').first():
-            User.create_user(user='harry', email='harry@hogwarts.com', password='secret')
-flask_app.run()
+        try:
+            if not User.query.filter_by(user_name='harry').first():
+                User.create_user(user='harry', email='harry@hogwarts.com', password='secret')
+        except exc.IntegrityError:
+            flask_app.run()
 
 
 # from app import create_app, db  # app/__init__ (ROOT PACKAGE)
@@ -20,3 +23,5 @@ flask_app.run()
 #         if not User.query.filter_by(user_name='harry').first():
 #             User.create_user(user='harry', email='harry@hogwarts.com', password='secret')
 #     flask_app.run()
+
+
